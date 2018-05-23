e826c6335930210326c5641bd04048192494b4bf
trieu <vuongtrieulk@gmail.com>
2018-05-23 22:17:36 +0700

    Increment and decrement line_item quantity

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,5 @@
 class LineItemsController < ApplicationController
-  before_action :set_line_item, only: [:update, :destroy]
+  before_action :set_line_item, except: [:create]
 
   def create
     food_item = FoodItem.find(params[:food_item_id])
@@ -19,6 +19,9 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
+        if @line_item.quantity <=0
+          @line_item.destroy
+        end
         format.html { redirect_to my_cart_path }
         format.json { render :show, status: :ok, location: @line_item }
       else
@@ -28,11 +31,24 @@ class LineItemsController < ApplicationController
     end
   end
 
+
+  def increase_quantity
+    @line_item.update_attribute(:quantity, @line_item.quantity+1)
+    redirect_to my_cart_path
+  end
+
+  def decrease_quantity
+    @line_item.update_attribute(:quantity, @line_item.quantity-1)
+    if @line_item.quantity <=0
+      @line_item.destroy
+    end
+    redirect_to my_cart_path
+  end
+
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.js {redirect_to new_order_url}
-      format.js
+      format.html { redirect_to my_cart_path }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :orders
   resources :line_items
+  patch 'line_item_increase/:id', to: 'line_items#increase_quantity', as:"line_item_increase"
+  patch 'line_item_decrease/:id', to: 'line_items#decrease_quantity', as:"line_item_decrease"
   resources :carts, only: [:destroy]
   get '/my_cart', to: 'carts#show'
   resources :food_items  do  	
